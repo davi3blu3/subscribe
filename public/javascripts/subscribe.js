6361b1ef0dd8a3ad8f5769f820d1c1c9b8c514be
Dave Harned <dharned@gmail.com>
2016-03-20 15:21:57 -0500

    first commit

---
+++ public/javascripts/subscribe.js
@@ -0,0 +1,44 @@
+'use strict'
+
+$(document).ready(function(){
+        
+    $('#submit').click(function formResults(){
+
+        // var info = {
+        //     "email_address": $("#email").val(),
+        //     "status": "subscribed",
+        //     "merge_fields": {
+        //         "FNAME": $("#firstName").val(),
+        //         "LNAME": $("#lastName").val()
+        //     }
+            
+        // };
+
+        // var infoObject = "body : " + JSON.stringify(info);
+        // console.log(infoObject);
+                 
+        // var xhr = new XMLHttpRequest();
+        // xhr.open('GET', 'localhost:3000', true);
+
+        // xhr.onreadystatechange = function () {
+        //     if (xhr.readyState === 4) {
+        //         if (xhr.status === 200) {
+        //             //document.getElementById('join').innerHTML = xhr.responseText;
+        //             console.log(xhr.responseText);
+        //         } else { 
+        //             console.log(xhr.statusText);
+        //         };
+        //     };
+        // };
+        
+        // xhr.send();
+
+        function success() {
+            console.log("Transmission successful!");
+        }
+
+
+
+
+    }) // end click
+}); // end ready    
